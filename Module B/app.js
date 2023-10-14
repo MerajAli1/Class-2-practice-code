cefe42b1fe569293ceba980ce6b1c99868b1ddd7
Meraj Ali <96920684+MerajAli1@users.noreply.github.com>
2023-10-14 22:00:08 +0500

    Add files via upload

---
+++ Module B/app.js
@@ -0,0 +1,102 @@
+//Spread Operator
+//Arrow Function
+//map,fiter,forEach
+//Destructring
+//hof
+//value & refrence
+//??
+
+// let  a = 10
+// let  b = a
+// b=22
+// let  arr = [1,2,3,4]
+// let  arr1 = arr
+
+// console.log(b);
+
+// const obj = {
+//     userName : "khan",
+//     id : "24635870"
+// }
+// const {userName,id} = obj;
+// console.log(userName,id);
+
+// const obj = {
+//   userName: "Meraj",
+//   details: {
+//     fatherName: "Zahid",
+//     Gender: "Male",
+//     contact: "98798698698",
+//   },
+// };
+// const {fatherName} = obj.details;
+// console.log(fatherName);
+
+// const arr = [
+//   "Emad",
+//   [1, 2, "Meraj", 3, 4, 4],
+//   {
+//     city: "London",
+//     state: "England",
+//   },
+// ];
+// const [,,a]= arr[1];
+// console.log(a);
+
+// const obj = {
+//   userName: "Meraj Ali",
+//   address: {
+//     city: "London",
+//     state: "England",
+//   },
+//   address2: {
+//     city: "London",
+//     state: "England",
+//   },
+//   address3: {
+//     city: "London",
+//     state: "England",
+//   },
+//   address4: {
+//     city: "London",
+//     state: "England",
+//   },
+// };
+
+// const keys = Object.keys(obj)
+// const values = Object.values(obj)
+// console.log(values)
+
+// const obj = {
+//   personalInfo: {
+//     name: "Meraj",
+//     age: 25,
+//   },
+//   address: {
+//     city: "london",
+//     state: "England",
+//   },
+// };
+
+// const keys = Object.keys(obj)
+// const values = Object.values(obj)
+// console.log(keys)
+// console.log(values)
+// for (let index = 0; index < values.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+// let a = ()=>{
+//     console.log("hi");
+// }
+// a()
+
+// abc()
+// function abc(){
+//     console.log("hi");
+// }
+
+
+
